5c91df1d4de0d268d5dbb605d8838c352adeca5e
Savandriel <bohdan.kaluha@techmagic.co>
2024-05-27 17:22:49 +0300

    intermediate commit

--- force-app/main/default/lwc/modalCandidateNew/modalCandidateNew.js
+++ force-app/main/default/lwc/modalCandidateNew/modalCandidateNew.js
@@ -1,12 +1,20 @@
 import { LightningElement, track, wire } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 import getFieldSetNamesWithPaths from '@salesforce/apex/MetadataControllerLWC.getFieldSetNamesWithPaths';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class ModalCandidateNew extends NavigationMixin(LightningElement) {
 
     @track isModalOpen;
     @track candidateFields;
     @track jobApplicationFields;
+    newCandidateId;
+    jaFieldsAreEmpty = true;
+    candidateForm;
+    jaForm;
+    hiddenCandidateSubmit;
+    hiddenJASubmit;
+    hasRendered = false;
 
     @wire(getFieldSetNamesWithPaths, {sObjectName: 'Candidate__c', fieldSetName: 'Candidate_Modal_New'})
     wiredCandidateFields({error, data}){
@@ -32,6 +40,17 @@ export default class ModalCandidateNew extends NavigationMixin(LightningElement)
 
     connectedCallback(){
         this.isModalOpen = true;
+        
+    }
+
+    renderedCallback(){
+        if(!this.hasRendered){
+            this.candidateForm = this.template.querySelector('.candidate-form');
+            this.jaForm = this.template.querySelector('.ja-form');
+            this.hiddenCandidateSubmit = this.template.querySelector('.candidate-form__hidden-submit');
+            this.hiddenJASubmit = this.template.querySelector('.ja-form__hidden-submit');
+            this.hasRendered = true;
+        }
     }
 
     openModal(){
@@ -53,15 +72,62 @@ export default class ModalCandidateNew extends NavigationMixin(LightningElement)
         })
     }
 
+    navigateToCandidateRecordPage(){
+        this[NavigationMixin.Navigate]({
+            type: "standard__recordPage",
+            attributes: {
+                recordId: this.newCandidateId,
+                objectApiName: "Candidate__c",
+                actionName: "view"
+            }
+        })
+    }
+
     handleSubmitForm(){
-        this.template.querySelector('.candidate-form__hidden-submit').click();
+        this.hiddenCandidateSubmit.click();
+        this.hiddenJASubmit.click();
     }
 
 
     handleCandidateSubmit(event){
         event.preventDefault();
-        console.log('Fields')
+        console.log('Candidate Fields')
         console.log(event.detail.fields);
-        event.target.submit();
+        console.log(this.template.querySelectorAll(".ja-form lightning-input-field"));
+        this.jaFieldsAreEmpty = Object.values(this.template.querySelectorAll(".ja-form lightning-input-field")).every(el => {
+            console.log(el.value);
+            return !el.value}
+        );
+        console.log(this.jaFieldsAreEmpty);
+        if(this.jaFieldsAreEmpty){
+            event.target.submit();
+        }
+    }
+
+    handleJASubmit(event){
+        event.preventDefault();
+        console.log('JA Fields')
+        console.log(event.detail.fields);
+        // this.jaFieldsAreEmpty = Object.values(event.detail.fields).every(value => !value);
+        // console.log(this.jaFieldsAreEmpty);
+    }
+
+    handleCandidateSuccess(event){
+        this.showToast('Success!', 'Candidate was successfully created', 'success', 'dismissable');
+        this.newCandidateId = event.detail.id;
+        console.log()
+        if(this.newCandidateId){
+            this.navigateToCandidateRecordPage();
+        }
+    }
+
+    showToast(title, message, variant, mode) {
+        const evt = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant,
+            mode: mode
+        });
+        this.dispatchEvent(evt);
     }
 }
